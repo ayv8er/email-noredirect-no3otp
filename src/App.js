07f2fe26a7d4bfa25fda38f5f0db0c3cc0b87b93
Richard Oh <richard@magic.link>
2024-07-25 01:28:13 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { magic } from "./magic";
+import { useEffect, useState } from "react";
+import "./index.css";
 
-function App() {
+export default function App() {
+  const [email, setEmail] = useState("");
+  const [user, setUser] = useState(null);
+
+  const handleLogin = async () => {
+    try {
+      await magic.auth.loginWithMagicLink({ email });
+      getUserMetadata();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getUserMetadata = async () => {
+    try {
+      const data = await magic.user.getInfo();
+      setUser(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      magic.user.logout();
+      setUser(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getUserMetadata();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>{user ? user.email : "Not logged in"}</h1>
+      {user ? (
+        <div>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button onClick={handleLogin}>Login</button>
+        </div>
+      )}
+      <div className="description">
+        <h3>Magic link + No Redirect + No 3 Digit OTP</h3>
+        <h4>
+          User submits email, then receives emailed magic link. User clicks link
+          and logs in. No user protection against phishing. Notice session is
+          issued where flow initiates.
+        </h4>
+      </div>
+    </main>
   );
 }
-
-export default App;
---
+++ src/magic.js
@@ -0,0 +1,3 @@
+import { Magic } from "magic-sdk";
+
+export const magic = new Magic("pk_live_95C27DE080E7065C");
